cab4e757a6c3b7ca59a7a473df1635a848c33af5
AltanGuevenBerlin <altan.gueven84@gmail.com>
2024-11-24 16:22:53 +0100

    Add the sun

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello, Mars!");
         System.out.println("Hello, Jupiter!");
+        System.out.println("Hello, Sun!");
 
     }
 }
